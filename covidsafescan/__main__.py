51ddf217ab5a0a2cdf1c717a13b93619bfe058e5
Michael Farrell <micolous+git@gmail.com>
2020-05-09 11:10:25 +1000

    Refactoring and adding support for staging UUID:
    * Adds support for the staging service UUID, used by debug builds of
      TraceTogether.
    
    * Adds support for scanning by using Withings manufacturer data, and showing
      the contents of it (CovidSafe privacy bug).
    
    * Refactor argparse to use `store_true`
    
    * Use f-strings, because it's the future.

--- covidsafescan/__main__.py
+++ covidsafescan/__main__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import base64
 import bleak
 import asyncio
 import traceback
@@ -7,20 +8,24 @@ import sys
 import datetime
 import json
 
-UUID="b82ab3fc-1595-4f6a-80f0-fe094cc218f9"
+WITHINGS_ID = 1023
+STAGING_UUID = '17e033d3-490e-4bc9-9fe8-2f567643f4d3'
+PRODUCTION_UUID = 'b82ab3fc-1595-4f6a-80f0-fe094cc218f9'
 
-async def connect(address, loop):
+
+async def connect(loop, address, uuid):
     async with bleak.BleakClient(address, loop=loop, timeout=args.timeout) as client:
-        message = await client.read_gatt_char(UUID, timeout=args.timeout)
+        message = await client.read_gatt_char(uuid, timeout=args.timeout)
+        now = datetime.datetime.now().isoformat()
         if args.json: #soooo deeeeep . what is pep8?
             data = {
-                "time": datetime.datetime.now().isoformat(),
-                "data":  message.decode("utf-8"),
+                "time": now,
+                "data": message.decode(),
                 "address": address
             }
             print(json.dumps(data))
         else:
-            print("[" + datetime.datetime.now().isoformat() + "] " + address + " : " + message.decode("utf-8"))
+            print(f'[{now}] {address} : {message.decode()}')
 
 def log(message):
     if args.debug:
@@ -33,35 +38,69 @@ async def run(loop):
         log("Found devices")
         log(", ".join([x.address for x in devices]))
         for d in devices:
-            try:
-                if UUID in d.metadata['uuids']:
-                    log("Connecting to " + d.address)
-                    try:
-                        result = await connect(d.address, loop)
-                        if result == False:
-                            log("Time out connecting")
-                    except KeyboardInterrupt:
-                        raise
-                    except: # ignore errors - yolo driven dev
-                        if args.debug:
-                            traceback.print_exc(file=sys.stderr)
-            except KeyError:
-                pass
+            log(f'{d.address}: {d.metadata}')
+            uuid = None
+
+            if args.adv_uuids and 'uuids' in d.metadata:
+                if PRODUCTION_UUID in d.metadata['uuids']:
+                    log('* Detected production TraceTogether UUID')
+                    uuid = PRODUCTION_UUID
+                elif STAGING_UUID in d.metadata['uuids']:
+                    log('* Detected staging TraceTogether UUID')
+                    uuid = STAGING_UUID
+            
+            if args.adv_manuf and 'manufacturer_data' in d.metadata:
+                manufacturer_data = d.metadata['manufacturer_data']
+                if WITHINGS_ID in manufacturer_data:
+                    withings_data = manufacturer_data[WITHINGS_ID]
+                    log(f'* Detected Withings manufacturer data: {base64.b16encode(withings_data).decode()} ({withings_data})')
+                    # TODO: Find the actual UUID to use. For now, assume prod.
+                    if uuid is None:
+                        uuid = PRODUCTION_UUID
+
+            if uuid is not None:
+                log("Connecting to " + d.address)
+                try:
+                    result = await connect(loop, d.address, uuid)
+                    if not result:
+                        log("Time out connecting")
+                except KeyboardInterrupt:
+                    raise
+                except: # ignore errors - yolo driven dev
+                    if args.debug:
+                        traceback.print_exc(file=sys.stderr)
         if args.once:
             break
 
 parser = argparse.ArgumentParser(description='Covidsafe BLE Scanner')
-parser.add_argument('--debug', dest='debug', action='store_const',
-                   const=True, default=False,
-                   help='Enables logs')
-parser.add_argument('--json', dest='json', action='store_const',
-                   const=True, default=False,
-                   help='JSON Output')
-parser.add_argument('--timeout', type=int, dest='timeout', default=15,
-                   help='JSON Output')
-parser.add_argument('--once', dest='once', action='store_const',
-                   const=True, default=False,
-                   help='Only run once')
+parser.add_argument(
+    '--debug',
+    action='store_true',
+    help='Enables logs')
+parser.add_argument(
+    '--json',
+    action='store_true',
+    help='JSON Output')
+parser.add_argument(
+    '--timeout',
+    type=int,
+    dest='timeout',
+    default=15,
+    help='Timeout, in seconds (default: %(default)s)')
+parser.add_argument(
+    '--once',
+    action='store_true',
+    help='Only run once')
+parser.add_argument(
+    '--no-adv-uuids',
+    dest='adv_uuids', action='store_false',
+    help='Don\'t use UUIDs in advertisement frames to find CovidSafe')
+
+parser.add_argument(
+    '--no-adv-manuf',
+    dest='adv_manuf', action='store_false',
+    help='Don\'t use Withings Manufacturer Data in advertisement frames to find CovidSafe')
+
 args = parser.parse_args()
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run(loop))
