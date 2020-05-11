fcb690040d40282307294a5931c23c5f3a59ccdf
xssfox <git@sprocketfox.io>
2020-05-11 18:30:52 +1000

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
       name='covidsafescan',
-      version='1.13',
+      version='1.14',
       description='Covid Safe Scanner',
       long_description=long_description,
       long_description_content_type="text/markdown",
